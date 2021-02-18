51d9248ccae5eeb289dd85f61a48349eb50fdad5
knowuchiha <knowuchiha@gmail.com>
2021-02-18 14:30:53 +0530

    make it better

--- common/config.js
+++ common/config.js
@@ -1,9 +1,10 @@
-let config = {};
+let config   = {};
+let MONGODB_URI = 'mongodb+srv://knowuchiha2881:Nayan$2881@cluster0.t6izx.mongodb.net/currency?retryWrites=true&w=majority' 
 
 config.app_name         = "localhost";
 config.app_port         = 8000;
 
 /* Mongo db url */
-config.mongo_url        = process.env.MONGODB_URI || 'mongodb://localhost:27017/currency';
+config.mongo_url        = MONGODB_URI || 'mongodb://localhost:27017/currency';
 
 module.exports = config;
