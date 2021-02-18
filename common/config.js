377efe62de0b4dc5b816af92cf6daf42c85c6114
knowuchiha <knowuchiha@gmail.com>
2021-02-18 13:59:58 +0530

    added setup

--- common/config.js
+++ common/config.js
@@ -4,6 +4,6 @@ config.app_name         = "localhost";
 config.app_port         = 8000;
 
 /* Mongo db url */
-config.mongo_url        = 'mongodb://localhost:27017/currency';
+config.mongo_url        = process.env.MONGODB_URI || 'mongodb://localhost:27017/currency';
 
 module.exports = config;
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 8000;
+const port = process.env.port | 8000;
 var path   = require ('path');
 const apis = require ('./routes/apis');
 
