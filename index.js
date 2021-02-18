bab39795b7ef98a368382e824185b2b93cdb4e14
knowuchiha <knowuchiha@gmail.com>
2021-02-18 16:13:47 +0530

    ss

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ const apis = require ('./routes/apis');
 
 app.get('/apis/*',apis);
 
-app.use(express.static(__dirname + "/reactapps/build"));
+app.use(express.static(__dirname + "reactapps/build"));
 
 app.use('/',(req, res) => {
-	return res.sendFile(path.join(__dirname + '/reactapps/build', 'index.html'));
+	return res.sendFile(path.join(__dirname + 'reactapps','build', 'index.html'));
 });
 
 app.use('/ping', (req,res) => res.send('pong'));
